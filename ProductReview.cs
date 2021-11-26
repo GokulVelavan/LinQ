fb3737f2e8a6d1bba06762eb1c29d20dab29941a
GokulVelavan <gokulvelavans@gmail.com>
2021-11-26 07:54:47 +0530

    welcome message

---
+++ ProductReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int userID { get; set; }
+        public float Rating { get; set; }
+
+        public string Review { get; set; }
+
+        public bool IsLike { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        //UC1
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to LinQ");
+            Console.ReadLine();
+        }
+
+       
+         
+    }
+}
